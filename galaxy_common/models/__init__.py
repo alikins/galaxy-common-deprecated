665ec9c3d37f3a10d76339fef1bb66dcbcef7293
Adrian Likins <alikins@redhat.com>
2019-06-27 14:57:28 -0400

    Add Namespace models for migrations

--- galaxy_common/models/__init__.py
+++ galaxy_common/models/__init__.py
@@ -1,3 +1,5 @@
 from .ping import Ping
 from .collection import Collection, CollectionVersion
 from .tag import Tag
+from .namespace import Namespace
+from .provider import Provider, ProviderNamespace
